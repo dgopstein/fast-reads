7438dc3a41b541cfbcccbfc3ca808ea53ef3b5f5
Dan Gopstein <dan@gopstein.com>
2014-11-02 03:35:00 -0500

    Download books from the index

---
+++ fast-reads.rb
@@ -0,0 +1,25 @@
+#! /usr/bin/ruby
+
+require 'open-uri'
+
+Book = Struct.new(:title, :url, :pages, :rating) do
+  def update_info
+    html = open(url)
+    rating_r = %r|<span class="average" itemprop="ratingValue">([^<])</span>|
+  end
+end
+
+def book_list
+  url = 'http://www.goodreads.com/list/show/264.Books_That_Everyone_Should_Read_At_Least_Once'
+  html = open(url).read
+  #regex = %r|<span itemprop="name">([^<]*)</span>| # Double-quotes around "name" returns authors, not titles!!!
+  url_r = %r|<a href="([^"*]*)" class="bookTitle" itemprop="url">|
+  title_r = %r|<span itemprop='name'>([^<]*)</span>|
+  book_r = /#{url_r}.*?#{title_r}/m
+  matches = html.scan(book_r)
+
+  books = matches.map{|tup| Book.new(*tup.reverse)}
+end
+
+book_list.each{
+
